072594feaac38df3fed784d1cc0417f1ba7da7e7
evgenia-fienik <160489180+evgenia-fienik@users.noreply.github.com>
2025-11-14 16:21:02 +0200

    Update Join section styles and logic

--- components/Join/Join.tsx
+++ components/Join/Join.tsx
@@ -1,5 +1,55 @@
-export default function Join() {
+"use client";
+
+import React from "react";
+import styles from './Join.module.css';
+import { useRouter } from "next/navigation";
+
+
+
+interface JoinProps {
+    isAuthenticated: boolean;
+}
+
+export default function Join({ isAuthenticated }: JoinProps) {
+    
+    const router = useRouter();
+
+    const handleClick = () => {
+        if(isAuthenticated) {
+            router.push('/auth/profile');
+        }else{
+            router.push('./auth/register')
+        }
+    }
     return (
-        <h2>Join</h2>
+        <section className={styles.joinSection}>
+    <div className={styles.imageContainer}>
+    <picture>
+      <source
+        srcSet="/images/join-mobile.jpg"
+        media="(max-width: 767px)"
+      />
+      <source
+        srcSet="/images/join-tab.jpg"
+        media="(max-width: 1439px)"
+      />
+      <source
+        srcSet="/images/join-desk.jpg"
+        media="(min-width: 1440px)"
+      />
+      <img
+        src="/images/join-mobile.jpg"
+        alt="Join section background"       
+        className={styles.backgroundImage}
+      />
+        </picture>
+            <div className={styles.content}>
+            <h2 className={styles.title}>Приєднуйтесь до нашої спільноти</h2>
+            <p className={styles.description}>Долучайтеся до мандрівників, які діляться своїми історіями та надихають на нові пригоди.</p>
+            <button className={styles.button } onClick={handleClick}>{isAuthenticated ? "Збереженні" : "Зареєструватися" }</button>
+            </div>
+     </div>
+        </section>
+       
     );
 }
