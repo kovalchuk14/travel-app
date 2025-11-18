d2a29e4480d980ead3290722a48cb49886afd0d3
AndriyTkh <andriytkh@icloud.com>
2025-11-18 08:47:32 +0200

    fixed authNavigation

--- components/AuthNavigation/AuthNavigation.tsx
+++ components/AuthNavigation/AuthNavigation.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/lib/store/authStore";
 import { logoutUser } from "@/lib/api/clientApi";
 import css from "./AuthNavigation.module.css";
+import Image from "next/image";
 
 export default function AuthNavigation() {
   const { isAuthenticated, user, clearIsAuthenticated } = useAuthStore();
@@ -15,6 +16,7 @@ export default function AuthNavigation() {
     } catch (error) {
       console.error("Logout failed:", error);
     } finally {
+      router.push("/");
       clearIsAuthenticated();
     }
   };
@@ -24,9 +26,27 @@ export default function AuthNavigation() {
 
     return (
       <nav className={css.navogationList}>
+        <Image
+          src={user.avatarUrl || "/images/auth-no-avatar.jpg"}
+          width={40}
+          height={40}
+          alt="User avatar"
+          className={css.userAvatar}
+        />
         <span className={css.userEmail}>{user.name}</span>
+        <div className={css.separator}></div>
         <button onClick={handleLogout} className={css.logoutButton}>
-          Logout
+          <svg
+            // className={css.aboutIcon}
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <use
+              // className={css.aboutIconSvg}
+              href="/icon.svg#icon-logout"
+            />
+          </svg>
         </button>
       </nav>
     );
