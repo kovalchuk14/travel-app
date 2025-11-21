4c2578249481dac6ad4b0a575aba52761410e88a
AndriyTkh <andriytkh@icloud.com>
2025-11-21 18:24:25 +0200

    changed 2 tags to work

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,18 +1,21 @@
-import css from '../../app/css/Hero.module.css';
+import css from "../../app/css/Hero.module.css";
 
 const Hero = () => {
   return (
     <section className={css.hero}>
-            <div className={css.content}>
-              <h1 className={css.title}>
-                Відкривайте світ <br/> подорожей з нами!
-              </h1>
-              <p className={css.subtitle}>
-                Приєднуйтесь до нашої спільноти мандрівників, де ви зможете ділитися своїми історіями та отримувати натхнення для нових пригод. Відкрийте для себе нові місця та знайдіть однодумців!</p>
-                <a href="#aboutus" className={css.moreButton}>Доєднатися</a>
-              </div>
-            <a href="#aboutus" className={css.moreButton}>Доєднатися</a>
-            </div>
+      <div className={css.content}>
+        <h1 className={css.title}>
+          Відкривайте світ <br /> подорожей з нами!
+        </h1>
+        <p className={css.subtitle}>
+          Приєднуйтесь до нашої спільноти мандрівників, де ви зможете ділитися
+          своїми історіями та отримувати натхнення для нових пригод. Відкрийте
+          для себе нові місця та знайдіть однодумців!
+        </p>
+        <a href="#aboutus" className={css.moreButton}>
+          Доєднатися
+        </a>
+      </div>
     </section>
   );
 };
