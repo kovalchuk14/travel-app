6accf850d1bfe23399d35a8d866fc0a729e387b0
AndriyTkh <andriytkh@icloud.com>
2025-11-22 18:29:33 +0200

    uncommented route

--- app/api/users/saved-articles/[articleId]/route.ts
+++ app/api/users/saved-articles/[articleId]/route.ts
@@ -1,48 +1,48 @@
-// import { NextResponse } from "next/server";
-// import { api } from "@/lib/api/api";
-// import { cookies } from "next/headers";
-// import { logErrorResponse } from "@/app/api/_utils/utils";
-// import { isAxiosError } from "axios";
+import { NextResponse } from "next/server";
+import { backendAPI } from "@/lib/backendAPI";
+import { cookies } from "next/headers";
+import { logErrorResponse } from "@/app/api/_utils/utils";
+import { isAxiosError } from "axios";
 
-// type Props = {
-//   params: { storyId: string };
-// };
+type Props = {
+  params: { storyId: string };
+};
 
-// export async function POST(request: Request, { params }: Props) {
-//   try {
-//     const cookieStore = await cookies();
-//     const accessToken = cookieStore.get("accessToken")?.value;
+export async function POST(request: Request, { params }: Props) {
+  try {
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get("accessToken")?.value;
 
-//     const res = await api.post(
-//       `/saved-stories/${params.storyId}`,
-//       {},
-//       {
-//         headers: {
-//           Cookie: cookieStore.toString(),
-//           Authorization: `Bearer ${accessToken}`,
-//         },
-//       }
-//     );
+    const res = await backendAPI.post(
+      `/saved-stories/${params.storyId}`,
+      {},
+      {
+        headers: {
+          Cookie: cookieStore.toString(),
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
 
-//     return NextResponse.json(res.data, { status: res.status });
-//   } catch (error) {
-//     if (isAxiosError(error)) {
-//       logErrorResponse(error.response?.data);
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
 
-//       return NextResponse.json(
-//         {
-//           error: error.message,
-//           response: error.response?.data,
-//         },
-//         { status: error.response?.status || 500 }
-//       );
-//     }
+      return NextResponse.json(
+        {
+          error: error.message,
+          response: error.response?.data,
+        },
+        { status: error.response?.status || 500 }
+      );
+    }
 
-//     logErrorResponse({ message: (error as Error).message });
+    logErrorResponse({ message: (error as Error).message });
 
-//     return NextResponse.json(
-//       { error: "Internal Server Error" },
-//       { status: 500 }
-//     );
-//   }
-// }
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
